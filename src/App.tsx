cad4ef12ab246a0bba9c200a2097968e0b4e2604
ccj-007 <596487930@qq.com>
2023-09-27 21:53:18 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { Stage, Layer, Rect, Text } from 'react-konva';
+import Konva from 'konva';
+
+const ColoredRect = () => {
+  const [color, setColor] = useState('green');
+
+  const handleClick = () => {
+    setColor(Konva.Util.getRandomColor());
+  };
+
+  return (
+    <Rect
+      x={20}
+      y={20}
+      width={50}
+      height={50}
+      fill={color}
+      shadowBlur={5}
+      onClick={handleClick}
+    />
+  );
+};
+
+const App = () => {
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Text text="Try click on rect" />
+        <ColoredRect />
+      </Layer>
+    </Stage>
+  );
+};
+
+export default App
